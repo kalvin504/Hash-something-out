547f0315f2a15dc96b3c1d6094919cd8980c8ee0
kalvin504 <kalvin.kheang@maine.edu>
2026-04-03 21:28:57 -0400

    Update hashTitle.py
    This is Attempt 4 of the movie title as the key hash table.
    Going into this i wanted to try to decrease the waste space by setting a more accurate tracker using used_buckets
    I also changed the table size from *2 to *1.3 so it uses less empty slots
    for optimizing the collisions better I kept the linear probing but 
    changed the hash function to use all letters instead of two.
    ended up with this:
    collsion: around 44000
    wasted space: 4500
    construction time: 0.01 
    for the next attempt I will try linked list again with these more optimized function

--- hashTitle.py
+++ hashTitle.py
@@ -1,16 +1,17 @@
 import time
 
 
-#creates hash table
+# Create a hash table with fixed size
 def hash_table(size):
     return [None] * size  #empty
 
-# Hash function first 2 letters
+# Hash function 
+#use entire title for better distribution
 def hash_function(key, size):
-    return hash(key[:2]) % size
+    return hash(key) % size
 
-# Insert using linear probing
-def insert(table, key, value, collisions):
+# Insert using linear probing 
+def insert(table, key, value, collisions, used_buckets):
     index = hash_function(key, len(table))
     start_index = index #remeber starting index
     while table[index] is not None:
@@ -19,18 +20,13 @@ def insert(table, key, value, collisions):
         if index == start_index:
             print("Warning Hash table is full", key)
             break
-    else: # only insert if we found an empty spot
-        table[index] = value #empty bucket then insert value
+    if table[index] == None:
+        table[index] = value
+        used_buckets.add(index)  # track used bucket
 
-#Counts empty buckets (wasted space)
-def wasted_space(table):
-    count = 0
-    for bucket in table:
-        if bucket == None:
-            count += 1
-    return count
-
-
+# Count wasted space by subtracting used buckets from table size
+def wasted_space(table, used_buckets):
+    return len(table) - len(used_buckets)
 
 movies = []
 
@@ -43,21 +39,22 @@ with open("MOCK_DATA.csv", "r", errors="ignore") as file:
         movies.append(title) # add title to list
 
 
+size = int(len(movies) * 1.3) # smaller table to reduce waste
+title_table = hash_table(size) #creates hash table
+title_collisions = [0]
+used_buckets = set() # track which buckets are used
 
-size = len(movies) * 2 #double the number of buckets to try to reduce collisions
-title_table = hash_table(size) #creates hash table 
-title_collisions = [0] #collisions counter
-
-start = time.time() #starts time
+start = time.time()
 
 for title in movies:
-    insert(title_table, title, title, title_collisions) #inserts title into hash table 
+    insert(title_table, title, title, title_collisions, used_buckets) #inserts title into hash table 
 
-end = time.time() #end time
+end = time.time()
 
 
 
-print("Attempt 3 - Movie Title as Key Hash Table ")
+
+print("Attempt 4 - Movie Title as Key Hash Table")
 print("Collisions:", title_collisions[0])
-print("Wasted Space:", wasted_space(title_table))
+print("Wasted Space:", wasted_space(title_table, used_buckets))
 print("Construction Time:", end - start)
