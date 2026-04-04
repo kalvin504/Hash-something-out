0536a83bb9c993ff530c1c98170e13c3f19d2094
kalvin504 <kalvin.kheang@maine.edu>
2026-04-03 20:05:53 -0400

    Update hashTitle.py
    This is attempt two of movie title as key. 
    I focused on trying to make the collision less.
    I still sticked with linked list 
    Instead of using the first letter of the title in the hash function I used the length of the title. len(key) % size 
    This attempt the collisions is 3799504 
    Waste space: 29901
    Time: 0.165 seconds 
    The collision went down a lot but the waste barely went down.
    the construction time went down from 0.4 to 0.165

--- hashTitle.py
+++ hashTitle.py
@@ -1,20 +1,21 @@
 import time
 
+
 #creates hash table with size buckets 
 #each bucket is a list
 def hash_table(size):
-    return [[] for i in range(size)]
+    return [[] for _ in range(size)]  
 
-
+# New hash function for Attempt 2 use title length
 def hash_function(key, size):
-    return hash(key[:1]) % size
+    return len(key) % size #length of title 
 
 # Insert a key value pair into the hash table
 def insert(table, key, value, collisions):
-    index = hash_function(key, len(table))  # compute bucket index
-    for item in table[index]:   # loop through bucket to count collisions
-        collisions[0] += 1
-    table[index].append(value)  # add value to bucket
+    index = hash_function(key, len(table)) # compute bucket index
+    for item in table[index]:  # loop through bucket to count collisions
+        collisions[0] += 1 
+    table[index].append(value) # add value to bucket
 
 #Counts empty buckets (wasted space)
 def wasted_space(table):
@@ -24,32 +25,36 @@ def wasted_space(table):
             count += 1
     return count
 
+
+
 movies = []
 
 with open("MOCK_DATA.csv", "r", errors="ignore") as file:
     header = file.readline()  # skip header
-    title_index = header.strip().split(',').index("movie_title")  # find index of title column
+    title_index = header.strip().split(',').index("movie_title") # find index of title column
 
     for line in file:
         columns = line.strip().split(',') # split line by comma
-        title = columns[title_index] # split line by comma
-        movies.append(title)  # add title to list
+        title = columns[title_index]
+        movies.append(title) # add title to list
 
 
-size = len(movies)*2 #double the number of buckets to try to reduce collisions
+
+size = len(movies) * 2 #double the number of buckets to try to reduce collisions
 title_table = hash_table(size) #creates hash table 
 title_collisions = [0] #collisions counter
-
 
 start = time.time() #starts time
 
 for title in movies:
     insert(title_table, title, title, title_collisions) #inserts title into hash table 
 
-end = time.time() #end time 
+end = time.time() #end time
 
 
-print("Attempt 1 - Movie Title as Key Hash Table")
+
+
+print("Attempt 2 - Movie Title as Key Hash Table")
 print("Collisions:", title_collisions[0])
 print("Wasted Space:", wasted_space(title_table))
 print("Construction Time:", end - start)
